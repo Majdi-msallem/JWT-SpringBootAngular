6138b98fb16b89fbaf1a91e4188c9940c360f315
Majdi-msallem <majdi.msallem@esprit.tn>
2024-05-24 17:57:38 +0200

    jwtBackend

---
+++ book/src/test/java/com/sec/book/BookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sec.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
